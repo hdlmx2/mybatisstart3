97a6c348a943e06738818545d9cdd140f5f73a27
handong <hdlmx@outlook.com>
2017-10-18 22:04:16 +0800

    注解方式

---
+++ src/test/java/Main.java
@@ -0,0 +1,40 @@
+import cn.hdlmx.mybatis.dao.IUser;
+import cn.hdlmx.mybatis.models.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.Reader;
+
+public class Main {
+    private static SqlSessionFactory sqlSessionFactory;
+    private static Reader reader;
+    static {
+        try {
+            reader = Resources.getResourceAsReader("Configure.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            sqlSessionFactory.getConfiguration().addMapper(IUser.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static SqlSessionFactory getSession() {
+        return sqlSessionFactory;
+    }
+
+    public static void main(String[] args) {
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            IUser iuser = session.getMapper(IUser.class);
+            User user = iuser.getUserByID(1);
+            System.out.println("名字："+user.getName());
+            System.out.println("所属部门："+user.getDept());
+            System.out.println("主页："+user.getWebsite());
+        } finally {
+            session.close();
+        }
+    }
+}
+
